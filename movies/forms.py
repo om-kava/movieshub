aacf8cb4cb34aa3b44ae88624a258c536869d61a
om kava <jordn2005@gmail.com>
2025-12-20 00:23:25 +0530

    Fix registration and clean gitignore

--- movies/forms.py
+++ movies/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Movie
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class MovieForm(forms.ModelForm):
     class Meta:
         model = Movie
         fields = ['title', 'description', 'release_date']
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Movie
 from django.db.models import Q
-from .forms import MovieForm
+from .forms import MovieForm, RegisterForm
 from django.http import JsonResponse
 
 
@@ -76,18 +75,18 @@ def delete_movie(request, id):
     messages.success(request, "Movie deleted successfully.")
     return redirect('movie_list')
 
-
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, "Registration successful. Please login.")
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
 
     return render(request, 'register.html', {'form': form})
+
 
 
 def movies_api(request):
